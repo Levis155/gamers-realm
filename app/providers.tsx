c1c03699a88b20a4f72fc8ed1952f9b60f5e282b
Levis155 <levismbui@gmail.com>
2025-11-12 07:48:41 +0300

    Implement dark mode

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,8 +2,13 @@
 
 import { ReactNode } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeProvider } from "./components/ui/color-mode";
 import { system } from "@/theme";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <ChakraProvider value={system}>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider value={system}>
+      <ColorModeProvider forcedTheme="dark">{children}</ColorModeProvider>
+    </ChakraProvider>
+  );
 }
